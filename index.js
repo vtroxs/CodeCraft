9b6b33139970b022753e87f49ced69c280df09ad
Oksana-anaskO <oksanasmiiukha@gmail.com>
2023-10-06 15:26:02 +0300

    adaptive footer

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ text.innerHTML = text.innerText.split("").map(
   (char, i) =>
   `<span style="transform:rotate(${i*8.7}deg")>${char}</span>`
 ).join("");
+
+// footer width 576px
+function moveFooterForm() {
+  const footerForm = document.querySelector('.footer_form');
+  const footerZag = document.querySelector('.footer_zag');
+  const footerText = document.querySelector('.footer_text');
+
+  if (window.innerWidth <= 576) {
+    footerZag.insertAdjacentElement('afterend', footerForm);
+  } else {
+    footerText.insertAdjacentElement('afterend', footerForm);
+  }
+}
+window.addEventListener('load', moveFooterForm);
+window.addEventListener('resize', moveFooterForm);
